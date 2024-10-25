b15174686f64f92eac818488a5658717df1b65c0
AkelaDumindu <akeladumindu2000@gmail.com>
2024-10-26 01:38:28 +0530

    created login page

---
+++ Medex/src/com/akeladumindu/medex/controller/LoginFormController.java
@@ -0,0 +1,20 @@
+package com.akeladumindu.medex.controller;
+
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane txtPassword;
+    public JFXTextField txtEmail;
+    public JFXRadioButton rBtnDoctor;
+    public ToggleGroup accountType;
+
+    public void signInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createAccountOnAction(ActionEvent actionEvent) {
+    }
+}
